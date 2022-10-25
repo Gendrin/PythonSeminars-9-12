62702cf979b8756874e569df036627c84ca94f85
Gendrin <Gendrinvl@gmail.com>
2022-10-26 01:30:25 +0300

    Testing game pole

--- Seminar9/Test3.py
+++ Seminar9/Test3.py
@@ -5,19 +5,33 @@ from aiogram import Bot, Dispatcher, types
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 # Объект бота
-bot = Bot(token="5664442295:AAGp5Zv0mdVbKJCwRqd9Fc3qdI3zUobl-UM")
+API_TOKEN = '5664442295:AAGp5Zv0mdVbKJCwRqd9Fc3qdI3zUobl-UM'
+bot = Bot(token=API_TOKEN)
 #bot = Bot(token="12345678:AaBbCcDdEeFfGgHh")
 # Диспетчер
-dp = Dispatcher()
+#dp = Dispatcher()
+dp = Dispatcher(bot)
 
 # Хэндлер на команду /start
-@dp.message(commands=["start"])
+@dp.message_handler(commands=["start"])
 async def cmd_start(message: types.Message):
     await message.answer("Hello!")
+
+# Хэндлер на команду /test1
+@dp.message_handler(commands=["test1"])
+async def cmd_test1(message: types.Message):
+    await message.reply("Test 1")
+
+# Хэндлер на команду /test2
+@dp.message_handler(commands=["test2"])
+async def cmd_test2(message: types.Message):
+    await message.reply("Test 2")
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
     await dp.start_polling(bot)
 
+
 if __name__ == "__main__":
     asyncio.run(main())
+    #dp.message_handler().register(cmd_test2, commands=["test2"])
--- Seminar9/Test4.py
+++ Seminar9/Test4.py
@@ -1,4 +1,5 @@
 import telebot
+import Test5 as ts5
 
 # Создаем экземпляр бота
 bot = telebot.TeleBot('5664442295:AAGp5Zv0mdVbKJCwRqd9Fc3qdI3zUobl-UM')
@@ -7,9 +8,20 @@ bot = telebot.TeleBot('5664442295:AAGp5Zv0mdVbKJCwRqd9Fc3qdI3zUobl-UM')
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
     bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+
+@bot.message_handler(commands=["game"])
+def start(m, res=False):
+    bot.send_message(m.chat.id,ts5.draw_board(ts5.board))
+    bot.send_message(m.chat.id, 'Выберите число вашего хода!')
 # Получение сообщений от юзера
+
+# def Pole():
+#     bot.send_message(m.chat.id, ts5.draw_board(ts5.board))
+
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+    listStep=['1','2','3','4','5','6','7','8','9']
+    if message.text in listStep:
+        bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
 # Запускаем бота
 bot.polling(none_stop=True, interval=0)
