abc13062d2983738db5708b6ee0ea5e8439c3cad
Gendrin <Gendrinvl@gmail.com>
2022-10-28 07:18:47 +1000

    Добавление логирования.

--- Seminar9_3/main.py
+++ Seminar9_3/main.py
@@ -2,6 +2,7 @@
 import telebot
 import game as ts5
 from config import Token
+from logg import inStringLog as inLg
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -24,15 +25,13 @@ def start(m, res=False):
     global startGame
     count_step=1
     bot.send_message(m.chat.id, 'Новая игра!!')
+    inLg('Запущена новая игра!')
     ts5.init_board()
     bot.send_message(m.chat.id,ts5.draw_board(ts5.board))
     bot.send_message(m.chat.id, 'Выберите число вашего хода!')
     startGame=True
 
 # Получение сообщений от юзера
-
-# def Pole():
-#     bot.send_message(m.chat.id, ts5.draw_board(ts5.board))
 
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
@@ -47,16 +46,20 @@ def handle_text(message):
             #bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
             if ts5.tk_in(simbol,int(message.text))!=None:
                 bot.send_message(message.chat.id, ts5.draw_board(ts5.board))
+                inLg('Игрок играющий за' +'"'+simbol+'"'+' выбрал поле -> '+message.text)
                 win= ts5.check_win()
                 if win:
-                    bot.send_message(message.chat.id, 'Победили ' +win+' игра окончена!')
+                    bot.send_message(message.chat.id, 'Победили '+'"'+win+'"'+' игра окончена!')
+                    inLg('Победили ' +'"'+win+'"'+' игра окончена!')
                     startGame=False
                 if count_step==9:
                     bot.send_message(message.chat.id, 'Ничья игра окончена!')
+                    inLg('Ничья игра окончена!')
                     startGame=False
                 count_step+=1
             else:
                 bot.send_message(message.chat.id, 'Это поле уже занято! ' + message.text)
+                inLg('Попытка ввода уже занятого поля игроком играющим за '+'"'+simbol+'"'+' поле - '+message.text)
 # Запускаем бота
 bot.polling(none_stop=True, interval=0)
 
