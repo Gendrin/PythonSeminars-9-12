595622735444fd22ec580dcba30dd210bd3f0c17
Gendrin <Gendrinvl@gmail.com>
2022-10-29 21:08:47 +1000

    Bot calc interface menu

--- Seminar10/main.py
+++ Seminar10/main.py
@@ -3,9 +3,14 @@ from telegram.ext import Updater
 from config import Token
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
+from user_interface import TextStartMenu as TSM
+from user_interface import TextRationalMenu as TRM
+from user_interface import TextComplexMenu as TCM
+from user_interface import TextDivMenu as TDM
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
+status=0
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 updater = Updater(Token(), use_context=True)
@@ -13,15 +18,26 @@ updater = Updater(Token(), use_context=True)
 dispatcher = updater.dispatcher
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Я бот калькулятор, напиши мне!")
+    context.bot.send_message(chat_id=update.effective_chat.id,text=TSM())
 
-def echo(update, context):
-    text = 'ECHO: ' + update.message.text
-    # `update.effective_chat.id` - определяем `id` чата,
-    # откуда прилетело сообщение
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text=text)
+def ControlInText(update, context):
+    global status
+    text = update.message.text
+    if status==0 and text=='1':
+        context.bot.send_message(chat_id=update.effective_chat.id, text=TRM())
+        status+=1
+    if status==0 and text=='2':
+        context.bot.send_message(chat_id=update.effective_chat.id, text=TCM())
+        status +=2
+    if (status==1  or status==2) and text=='0':
+        context.bot.send_message(chat_id=update.effective_chat.id, text=TSM())
+        status=0
+    if status==1 and text=='4':
+        context.bot.send_message(chat_id=update.effective_chat.id, text=TDM())
+        status = 3
+    if status==3 and text=='0':
+        context.bot.send_message(chat_id=update.effective_chat.id, text=TRM())
+        status = 1
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -33,7 +49,7 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
     start_handler = CommandHandler('start', start)
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), ControlInText)
 
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(echo_handler)
